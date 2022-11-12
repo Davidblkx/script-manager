d321d106424e6e9a7767a04fe045f90cce13799b
David Pires <developer@davidpires.pt>
2022-11-12 20:59:15 +0000

    throw error when a invalid target is called

--- src/core/init/targets.init.ts
+++ src/core/init/targets.init.ts
@@ -2,12 +2,22 @@ import { ITargetHandler, TargetHandler } from "../../modules/targets.ts";
 import { DirHandler } from '../../modules/infra/dir-handler.ts';
 import { IConfigHandler } from "../../modules/config.ts";
 import { ISettingsManager } from "../../modules/settings.ts";
+import { logger } from "../../modules/logger.ts";
 
 export function initTargetHandler(
   configHandler: IConfigHandler,
   settings: ISettingsManager,
   targetId?: string,
 ): ITargetHandler {
+
+  if (targetId) {
+    const target = configHandler.localFile?.config.targets[targetId];
+    if (!target) {
+      logger.error(`Target ${targetId} not found`);
+      Deno.exit(1);
+    }
+  }
+
   return new TargetHandler(
     configHandler,
     settings,
